c17e142aa0728672ed24d2be5897923ad891799c
Arman997 <iperov.55@gmail.com>
2025-03-13 20:57:24 +0400

    I added the Makefile

--- app.py
+++ app.py
@@ -21,7 +21,3 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 
-# esel vor mi ip adress -ov baci 
-# if __name__ == '__main__':
-
-#     app.run(debug=True)     
